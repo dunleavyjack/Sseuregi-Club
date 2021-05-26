28aafafc7397b3703622834e9ea5d4ed8632b11b
dunleavyjack <dunleavyjack@gmail.com>
2021-05-26 20:39:41 +0900

    Added multiple markets

---
+++ src/data/markerData.js
@@ -0,0 +1,65 @@
+export const markerdata = [
+  {
+    title: "1 | Trash + Recyling",
+    lat: 37.491776,
+    lng: 127.054961,
+  },
+  {
+    title: "3 | Trash + Recyling",
+    lat: 37.487337,
+    lng: 127.032718,
+  },
+  {
+    title: "4 | Trash Only",
+    lat: 37.487152,
+    lng: 127.032786,
+  },
+  {
+    title: "5 | Trash + Recyling",
+    lat: 37.486539,
+    lng: 127.033071,
+  },
+  {
+    title: "6 | Trash Only",
+    lat: 37.484813,
+    lng: 127.034227,
+  },
+  {
+    title: "7  | Trash + Recyling",
+    lat: 37.469261,
+    lng: 127.039737,
+  },
+  {
+    title: "8",
+    lat: 37.501206,
+    lng: 127.036936,
+  },
+  {
+    title: "9",
+    lat: 37.513803,
+    lng: 127.030809,
+  },
+  {
+    title: "10",
+    lat: 37.513803,
+    lng: 127.030809,
+  },
+  {
+    title: "11",
+    lat: 37.514236,
+    lng: 127.030563,
+  },
+  {
+    title: "12",
+    lat: 37.514843,
+    lng: 127.030449,
+  },
+  {
+    title: "13",
+    lat: 37.518247,
+    lng: 127.028915,
+  },
+];
+
+// long = x
+// lat = y
