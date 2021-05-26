414839bf2b146c6035ee4feb2164154b1277b807
dunleavyjack <dunleavyjack@gmail.com>
2021-05-26 20:39:47 +0900

    Restyled

--- src/Components/map/Map.js
+++ src/Components/map/Map.js
@@ -1,5 +1,6 @@
 /*global kakao */
 import React, { useEffect } from "react";
+import { markerdata } from "../../data/markerData";
 
 export default function Map() {
   useEffect(() => {
@@ -9,25 +10,24 @@ export default function Map() {
   const mapscript = () => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+      center: new kakao.maps.LatLng(37.514843, 127.020449),
       level: 5,
     };
+
     //map
     const map = new kakao.maps.Map(container, options);
 
-    //마커가 표시 될 위치
-    let markerPosition = new kakao.maps.LatLng(
-      37.62197524055062,
-      127.16017523675508
-    );
-
-    // 마커를 생성
-    let marker = new kakao.maps.Marker({
-      position: markerPosition,
+    markerdata.forEach((el) => {
+      // 마커를 생성합니다
+      new kakao.maps.Marker({
+        //마커가 표시 될 지도
+        map: map,
+        //마커가 표시 될 위치
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        //마커에 hover시 나타날 title
+        title: el.title,
+      });
     });
-
-    // 마커를 지도 위에 표시
-    marker.setMap(map);
   };
 
   return <div id="map" style={{ width: "100vw", height: "100vh" }}></div>;
