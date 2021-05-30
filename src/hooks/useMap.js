80457dd46d6ddb379302577d07021a015db2225d
dunleavyjack <dunleavyjack@gmail.com>
2021-05-30 15:23:44 +0900

    Toggeling map icons

--- src/hooks/useMap.js
+++ src/hooks/useMap.js
@@ -1,7 +1,9 @@
 /*global kakao */
 import { useEffect } from 'react';
 import { markerdata } from '../data/markerData';
-import mapPin from '../assets/images/mapPin.png';
+import trashPinImage from '../assets/images/trashPin.png';
+import recyclingPinImage from '../assets/images/recyclingPin.png';
+import locationPinImage from '../assets/images/locationPin.png';
 import { getGridPosition } from '../utils/helperFunctions';
 
 const useMap = () => {
@@ -12,9 +14,29 @@ const useMap = () => {
     const mapscript = () => {
         let container = document.getElementById('map');
         let options = {
-            center: new kakao.maps.LatLng(37.51355, 127.033647),
-            level: 5,
+            center: new kakao.maps.LatLng(37.505809, 127.037707),
+            level: 7,
         };
+
+        const trashPinSrc = trashPinImage,
+            trashPinSize = new kakao.maps.Size(50, 75);
+
+        const recyclePinSrc = recyclingPinImage,
+            recyclePinSize = new kakao.maps.Size(50, 75);
+
+        const locationPinSrc = locationPinImage,
+            locationPinSize = new kakao.maps.Size(25, 37);
+
+        const trashPin = new kakao.maps.MarkerImage(trashPinSrc, trashPinSize);
+
+        const locationPin = new kakao.maps.MarkerImage(
+            locationPinSrc,
+            locationPinSize
+        );
+        const recyclePin = new kakao.maps.MarkerImage(
+            recyclePinSrc,
+            recyclePinSize
+        );
 
         const map = new kakao.maps.Map(container, options);
 
@@ -22,9 +44,15 @@ const useMap = () => {
             new kakao.maps.Marker({
                 map: map,
                 position: locPosition,
+                // image: locationPin,
             });
 
             map.setCenter(locPosition);
+            map.setLevel(5, {
+                animate: {
+                    duration: 500,
+                },
+            });
         };
 
         if (navigator.geolocation) {
@@ -40,15 +68,6 @@ const useMap = () => {
             displayMarker(locPosition);
         }
 
-        // const lattt = new kakao.maps.LatLng(37.491776, 127.054961);
-        // console.log(lattt.toCoords().toString());
-        // console.log(lattt.toCoords().toString().split(',')[0]);
-
-        const imageSrc = mapPin,
-            imageSize = new kakao.maps.Size(50, 75);
-
-        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
         markerdata.forEach((trashCan) => {
             let trashPosition = new kakao.maps.LatLng(
                 trashCan.lat,
@@ -60,13 +79,21 @@ const useMap = () => {
                 map: map,
                 position: trashPosition,
                 title: trashCan.title,
-                image: markerImage,
+                image: trashCan.recycling ? recyclePin : trashPin,
             });
             let infowindow = new kakao.maps.InfoWindow({
                 position: trashPosition,
                 content: `<div><a href="https://map.kakao.com/?urlX=${trashGridPosition.x}&urlY=${trashGridPosition.y}&name=Public+Trash+Can%21">Directions</a></div>`,
             });
-            infowindow.open(map, marker);
+            // infowindow.open(map, marker);
+            kakao.maps.event.addListener(marker, 'click', function () {
+                // 마커 위에 인포윈도우를 표시합니다
+                infowindow.open(map, marker);
+            });
+            kakao.maps.event.addListener(map, 'click', function () {
+                // 마커 위에 인포윈도우를 표시합니다
+                infowindow.close();
+            });
         });
     };
 };
