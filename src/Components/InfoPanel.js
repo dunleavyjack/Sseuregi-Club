9d84756319c1c0683d77b4c5273ee87c55de2b6a
dunleavyjack <dunleavyjack@gmail.com>
2021-06-02 10:53:21 +0900

    Resposive for laptop

--- src/Components/InfoPanel.js
+++ src/Components/InfoPanel.js
@@ -3,16 +3,18 @@ import React from 'react';
 const InfoPanel = ({ nearby, closest = 0 }) => {
     return (
         <>
-            <div className="infoPanel">
-                <div className="infoCircleContainer">
-                    <div className="infoCircle"></div>
-                    <div className="infoBar">
-                        <p className="infoText">NEAREST</p>
-                        <p className="infoDistance">{closest}m</p>
+            <div className="infoContainer">
+                <div className="infoPanel">
+                    <div className="infoCircleContainer">
+                        <div className="infoCircle"></div>
+                        <div className="infoBar">
+                            <p className="infoText">NEAREST</p>
+                            <p className="infoDistance">{closest}m</p>
+                        </div>
+                        <p className="info">
+                            <span className="number">{nearby}</span> NEARBY BINS
+                        </p>
                     </div>
-                    <p className="info">
-                        <span className="number">{nearby}</span> NEARBY BINS
-                    </p>
                 </div>
             </div>
         </>
